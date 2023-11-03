861c04215babc50f94958674fdc7a2eead1aa5ef
Munkherdene28 <b.munkherdene28@gmail.com>
2023-11-03 12:29:36 +0800

    new

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ function displayback() {
   note.classList.remove("flex");
 }
 away.onclick = displayback;
+
+function addCard() {
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  const status = document.getElementById("status").value;
+  const priority = document.getElementById("priority").value;
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+<div class="icon">
+<i class="fa fa-check" aria-hidden="true"></i>
+</div>
+<div class="texts">
+<h4>${title}</h4>
+<p>${description}</p>
+<div class="text">${priority}</div>
+</div>
+<div class="icons">
+<div class="icon">
+<i class="fa fa-close" aria-hidden="true"></i>
+</div>
+<div class="icon">
+<i class="fa fa-edit" aria-hidden="true"></i>
+</div>
+</div> `;
+  const panel = document.querySelector("div");
+  panel.querySelector(".cards").appendChild(card);
+}
