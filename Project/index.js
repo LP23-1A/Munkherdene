45d95d6ab23b8567341bf3292d7218446cad07af
Munkherdene28 <b.munkherdene28@gmail.com>
2023-11-17 12:40:42 +0800

    Complete

--- Project/index.js
+++ Project/index.js
@@ -2,8 +2,10 @@ const note = document.querySelector(".subcontainer");
 const add = document.querySelector(".btn");
 const away = document.querySelector(".away");
 const cardForm = document.getElementById("card-form");
-
 const statusSelect = document.getElementById("status");
+function generateRandomId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
 function openNote() {
   note.style.display = "flex";
 }
@@ -12,7 +14,6 @@ function closeNote() {
   note.style.display = "none";
 }
 away.addEventListener("click", closeNote);
-
 const cardsData = [];
 function addCard() {
   const title = document.getElementById("title").value;
@@ -20,6 +21,7 @@ function addCard() {
   const status = statusSelect.value;
   const priority = document.getElementById("priority").value;
   const newCard = {
+    id: generateRandomId(),
     title: title,
     description: description,
     status: status,
@@ -33,37 +35,35 @@ function addCard() {
 }
 function renderCards() {
   const cardsContainers = document.querySelectorAll(".cards");
-
   cardsContainers.forEach((cardsContainer) => {
     cardsContainer.innerHTML = "";
   });
-  cardsData.forEach((card, index) => {
-    const cardHtml = createCardHtml(card, index);
-
+  cardsData.forEach((card) => {
+    const cardHtml = createCardHtml(card);
     cardsContainers[getStatusIndex(card.status)].innerHTML += cardHtml;
   });
 }
-function createCardHtml(card, index) {
+function createCardHtml(card) {
   return `
-    <div class="card" draggable="true" id="test" data-id="${index}">
-        <div class="icon">
-            <i class="fa fa-check" onclick="movecard(${index})" aria-hidden="true"></i>
-        </div>
-        <div class="texts">
-            <h4>${card.title}</h4>
-            <p>${card.description}</p>
-            <div class="text">${card.priority}</div>
-        </div>
-        <div class="icons">
-            <div class="icon">
-                <i class="fa fa-close" aria-hidden="true" onclick="removeCard(${index})"></i>
-            </div>
-            <div class="icon">
-                <i class="fa fa-edit" aria-hidden="true" onclick="editCard(${index})"></i>
-            </div>
-        </div>
-    </div>
-    `;
+<div class="card" draggable="true" id="test" data-id="${card.id}">
+<div class="icon">
+<i class="fa fa-check" onclick="movecard('${card.id}')" aria-hidden="true"></i>
+</div>
+<div class="texts">
+<h4>${card.title}</h4>
+<p>${card.description}</p>
+<div class="text">${card.priority}</div>
+</div>
+<div class="icons">
+<div class="icon">
+<i class="fa fa-close" aria-hidden="true" onclick="removeCard('${card.id}')"></i>
+</div>
+<div class="icon">
+<i class="fa fa-edit" aria-hidden="true" onclick="editCard('${card.id}')"></i>
+</div>
+</div>
+</div>
+   `;
 }
 function getStatusIndex(status) {
   let statuses = ["To do", "In progress", "Stuck", "Done"];
@@ -73,14 +73,14 @@ renderCards();
 const lastCard = cardsData[cardsData.length - 1];
 if (lastCard) {
   const statusIndex = getStatusIndex(lastCard.status);
-  if (statusIndex == 1) {
-    const cardsContainer = document.querySelectorAll(".cards");
-    const cardHtml = createCardHtml(lastCard, cardsData.length - 1);
+  if (statusIndex !== -1) {
+    const cardsContainer = document.querySelector(`#${statuses[statusIndex]}`);
+    const cardHtml = createCardHtml(lastCard);
     cardsContainer.innerHTML += cardHtml;
   }
 }
-
-function removeCard(index) {
+function removeCard(id) {
+  const index = cardsData.findIndex((card) => card.id === id);
   cardsData.splice(index, 1);
   renderCards();
   updateCardsCount();
@@ -89,11 +89,10 @@ cardForm.addEventListener("submit", function (element) {
   element.preventDefault();
   addCard();
 });
-
 let editingCardIndex = null;
-function editCard(index) {
-  editingCardIndex = index;
-  const cardToEdit = cardsData[index];
+function editCard(id) {
+  editingCardIndex = cardsData.findIndex((card) => card.id === id);
+  const cardToEdit = cardsData[editingCardIndex];
   document.getElementById("title").value = cardToEdit.title;
   document.getElementById("description").value = cardToEdit.description;
   document.getElementById("status").value = cardToEdit.status;
@@ -107,14 +106,12 @@ function updateCard() {
   cardToEdit.description = document.getElementById("description").value;
   cardToEdit.status = document.getElementById("status").value;
   cardToEdit.priority = document.getElementById("priority").value;
-
   cardForm.reset();
   closeNote();
   editingCardIndex = null;
   renderCards();
   updateCardsCount();
 }
-
 document.getElementById("button").addEventListener("click", function (e) {
   e.preventDefault();
   if (editingCardIndex !== null) {
@@ -123,10 +120,9 @@ document.getElementById("button").addEventListener("click", function (e) {
     addCard();
   }
 });
-function movecard(index) {
-  const movecard = cardsData.splice(index, 1)[0];
+function movecard(id) {
+  const movecard = cardsData.find((card) => card.id === id);
   movecard.status = "Done";
-  cardsData.push(movecard);
   renderCards();
   updateCardsCount();
 }
@@ -163,15 +159,15 @@ panel.forEach((panel) => {
       if (draggingcards !== event.currentTarget) {
         event.currentTarget.querySelector(".cards").appendChild(draggedItem);
         let id = draggedItem.getAttribute("data-id");
-        data = data.map((card) => {
+        cardsData.map((card) => {
           if (card.id === id) {
-            if (event.currentTarget.id === "todo") {
+            if (event.currentTarget.id === "To do") {
               card.status = "To do";
-            } else if (event.currentTarget.id === "inprogress") {
+            } else if (event.currentTarget.id === "In progress") {
               card.status = "In progress";
-            } else if (event.currentTarget.id === "stuck") {
+            } else if (event.currentTarget.id === "Stuck") {
               card.status = "Stuck";
-            } else if (event.currentTarget.id === "done") {
+            } else if (event.currentTarget.id === "Done") {
               card.status = "Done";
             }
           }
